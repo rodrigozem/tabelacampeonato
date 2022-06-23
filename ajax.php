0cf0f94f7b7c5caadcbceaffc8113a19ca1f5806
Rodrigo <digaoguitarra@gmail.com>
2022-06-23 17:11:28 -0300

    Alterações para retirar jquery
    agora, somente javascript

--- ajax.php
+++ ajax.php
@@ -2,23 +2,26 @@
 require("TabelaCampeonato.class.php");
 $modulo2 = new TabelaCampeonato();
 
-$acao = $_REQUEST['acao'];
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body);
+
+$acao = $data->acao;
 
 switch($acao){
 
     case 'rodada':
-        $modulo2->setFaseAtual($_REQUEST['fase']);
+        $modulo2->setFaseAtual($data->fase);
 
-        $rodada = $modulo2->showJogosRodada($_REQUEST['goto']);
-        
-        echo $rodada;
+        $rodada = $modulo2->showJogosRodada($data->goto);
+                
+        echo json_encode(['result' => $rodada]);
         
         break;
     
     case 'fase':
-        $modulo2->setFaseAtual($_REQUEST['goto']);
+        $modulo2->setFaseAtual($data->goto);
         
-        $fase          = $modulo2->showFases($_REQUEST['goto']);        
+        $fase          = $modulo2->showFases($data->goto);        
         $rodada        = $modulo2->showJogosRodada($modulo2->getRodadaAtual());
         $classificacao = $modulo2->showClassificacao();
         $legenda       = $modulo2->showLegendaFase();
--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 require("TabelaCampeonato.class.php");
 
 $modulo2 = new TabelaCampeonato();
-//$modulo2->setFaseAtual(2);
 $fase = $modulo2->getFaseAtual();
 $rodada = $modulo2->showJogosRodada();
 $classificacao = $modulo2->showClassificacao();
 $legenda = $modulo2->showLegendaFase();
 $fase = $modulo2->getFaseDescricao($fase);
 $modulo2->getClubesFase();
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -19,60 +17,7 @@ $modulo2->getClubesFase();
     <title><?=mb_strtolower($modulo2->getNomeCampeonato());?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <style>
-        .material-symbols-outlined {            
-            font-variation-settings:
-            'FILL' 1,
-            'wght' 400,
-            'GRAD' 0,
-            'opsz' 48
-        }
-
-        .icon-xs { font-size:9px; margin-right:7px;}
-        .text-end { color: #ad9f9f; cursor:default;}
-
-        table { font-size: 11px; }
-        table th { font-weight:normal; }
-        table thead { height: 46px;}
-        table thead tr th:nth-child(even){ --bs-table-accent-bg: transparent!important;}
-        table tbody {font-size:13px;}
-        table tbody tr td:not(:first-child) { text-align:center;}
-        table thead tr th:not(:first-child) { text-align:center;}
-        table th:first-child { background-color: #FFF; position:sticky;left:0px;z-index:9999;}
-        table td:nth-child(2) {background-color: #FFF;position:sticky;left:0px;z-index:9999;}
-
-        .cla { font-size: 20px; width:20px; display:inline-block;}
-        .cla-gcla { color: blue; display:inline-block; font-size:20px; }
-        .cla-zcla { color: red; }
-        .cla-time { font-size: 18px;padding-left:20px; }
-        .cla-ultimos-jogos { border-radius: 50%;display: inline-block;height: 7px;margin-left: 2px;overflow: hidden;width: 7px; }
-        .cla-ultimos-jogos--v { background-color: #51a81e; }
-        .cla-ultimos-jogos--d { background-color: #f00; }
-        .cla-ultimos-jogos--e { background-color: #ccc; }        
-        .cla-ultimos-jogos--neutra { background-color: #ccc; }
-        .cla-title { width:40% }
-        .cla-title::after { width:10%;content:'CLASSIFICAÇÃO'}
-        .cla-box {
-            height:7px;
-            width:7px;
-            margin-right:5px;
-        }
-        
-        .jogos-info { font-size: 11px; }
-        .jogos-time img { height:30px;width:30px; }
-        .jogos-versus span { font-size:14px; }
-        .legenda { font-size: 11px; }
-        @media (max-width: 768px) {
-            .cla { font-size: 20px; width:50px; display:inline-block;}
-            .cla-ultimos-jogos { margin-left:1px;height:5px;width:5px;}
-            .cla-time { font-size: 17px;padding-left:5px; }
-            .cla-title { width:80% }
-            .cla-title::after { width:60%;content:'CLASSIFIC.'}
-            table tbody tr td:not(:first-child) { text-align:left; }
-            table thead tr th:not(:first-child) { text-align:left; }
-            .legenda { font-size: 11px; margin-top: 7px;margin-bottom: 15px;}
-        }
-    </style>
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <div class="container-xl mt-4 mb-4">
@@ -88,7 +33,7 @@ $modulo2->getClubesFase();
             </div>
             <div class="row">
             <div class="col-lg-8 col-12">
-                <div class="freeze">
+                <div id="freeze">
                     <table class="table border-top">
                         <thead class="text-muted">
                             <tr>
@@ -131,15 +76,10 @@ $modulo2->getClubesFase();
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="node_modules/js-loading-overlay/dist/js-loading-overlay.min.js"></script>
     <script>
-        
-        $(function(){
-            $(".freeze").css('overflowX','auto');
-            $(".disclaimer").remove();
-        })
-
+        document.querySelector(".disclaimer").remove();
+        document.getElementById("freeze").style.overflow = "scroll";
         function rodada(pos)
         {
             let elem = document.getElementById('nrodada');
@@ -151,19 +91,23 @@ $modulo2->getClubesFase();
             if(goto <= 0) goto = 1;
             if(goto > totalrodadas) goto = totalrodadas;
 
-            $.ajax({
-                url : "ajax.php",
-                type: "POST",
-                data: { acao:'rodada','goto':goto, 'fase':document.getElementById('nfase').getAttribute('data-nfase') },
-                beforeSend:function(){
-                    JsLoadingOverlay.show({containerID:'nrodada',spinnerSize: '1x',spinnerIcon:'ball-spin-clockwise'});
-                },
-                success: function(data)
-                {
-                    JsLoadingOverlay.hide();
-                    $("#rodada").html(data);
-                }
-            })
+            JsLoadingOverlay.show({containerID:'nrodada',spinnerSize: '1x',spinnerIcon:'ball-spin-clockwise'});
+
+            fetch("ajax.php", {
+                // Adding method type
+                method: "POST",
+                // Adding body or contents to send
+                body: JSON.stringify({
+                    acao:'rodada','goto':goto, 'fase':document.getElementById('nfase').getAttribute('data-nfase')
+                })
+            })            
+            // Converting to JSON
+            .then(response => response.json())
+            // Displaying results to console          
+            .then((data) => {
+                JsLoadingOverlay.hide();
+                document.getElementById("rodada").innerHTML = data.result;
+            });
         }
 
         function fase(pos)
@@ -177,25 +121,29 @@ $modulo2->getClubesFase();
             if(goto <= 0) goto = 1;
             if(goto > totalfases) goto = totalfases;
 
-            $.ajax({
-                url : "ajax.php",
-                type: "POST",
-                data: { acao:'fase','goto':goto },
-                dataType:'json',
-                beforeSend:function(){
-                    JsLoadingOverlay.show({containerID:'nfase',spinnerSize: '1x',spinnerIcon:'ball-spin-clockwise'});
-                },
-                success: function(data)
-                {
-                    elem.getAttribute('data-nfase',goto);
-                    JsLoadingOverlay.hide();
-                    $("#rodada").html(data.rodadaHtml);
-                    $("#fase").html(data.faseHtml);
-                    $("#classificacao").html(data.classificacaoHtml);
-                    $("#legenda-fase").html(data.legendaHtml);
-                    fase = elem.setAttribute("data-nfase",data.fase);
-                }
-            })
+            JsLoadingOverlay.show({containerID:'nrodada',spinnerSize: '1x',spinnerIcon:'ball-spin-clockwise'});
+
+            fetch("ajax.php", {
+                // Adding method type
+                method: "POST",
+                // Adding body or contents to send
+                body: JSON.stringify({
+                    acao:'fase','goto':goto
+                })
+            })            
+            // Converting to JSON
+            .then(response => response.json())
+            // Displaying results to console          
+            .then((data) => {
+                JsLoadingOverlay.hide();
+                elem.getAttribute('data-nfase',goto);
+                JsLoadingOverlay.hide();
+                document.getElementById("rodada").innerHTML = data.rodadaHtml;
+                document.getElementById("fase").innerHTML = data.faseHtml;
+                document.getElementById("classificacao").innerHTML = data.classificacaoHtml;
+                document.getElementById("legenda-fase").innerHTML = data.legendaHtml;
+                fase = elem.setAttribute("data-nfase",data.fase);
+            });
         }
     </script>
   </body>
